5871242af59c51abf571754061dc7ff827601f12
alexrives <alexrives@Alexanders-MacBook-Pro-2.local>
2014-09-18 11:59:26 -0700

    next

--- luajit-rocks.rb
+++ luajit-rocks.rb
@@ -4,7 +4,7 @@ class LuajitRocks < Formula
   head "https://github.com/torch/luajit-rocks.git", :branch => "master"
   homepage "https://github.com/torch/luajit-rocks"
 
-  depends_on "cmake" => :build
+  # depends_on "cmake" => :build
 
   def install
     system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
